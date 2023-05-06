7a18b7f6132509f692040ef9a55e4f6e95b44a4e
MarioKarv <mariook0204@gmail.com>
2023-05-06 11:08:14 +0300

    finished

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,4 +1,8 @@
-
+import PropTypes from 'prop-types';
 export function Notification({ message }) {
-    return <p>{message}</p>
+  return <p>{message}</p>;
 }
+
+Notification.propTypes = {
+  message: PropTypes.string.isRequired,
+};
